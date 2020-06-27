b1ba927beb64420f6b5696bd8449f2f42b032e66
arjunp <>
2020-06-27 17:11:06 +0530

    added loader

--- app/src/main/java/com/quastio/juno/activities/MainActivity.kt
+++ app/src/main/java/com/quastio/juno/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.quastio.juno.activities
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -14,8 +15,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.quastio.juno.R
 import com.quastio.juno.models.FotoApiResponseModel
 import com.quastio.juno.models.ResultWrapper
@@ -132,6 +137,8 @@ class MainActivity : AppCompatActivity() {
                                 videoId?.let {
                                     val requestOptions = RequestOptions()
                                     requestOptions.isMemoryCacheable
+                                    requestOptions.placeholder(R.drawable.ic_baseline_image_24)
+                                      requestOptions.error(R.drawable.ic_baseline_broken_image_24)
                                     Glide.with(this@MainActivity)
                                         .setDefaultRequestOptions(requestOptions)
                                         .load("https://img.youtube.com/vi/$videoId/0.jpg")
@@ -142,9 +149,34 @@ class MainActivity : AppCompatActivity() {
                             else -> {
                                 action_iv.setImageResource(R.drawable.ic_baseline_zoom_in)
                                 action_iv.tag = IMAGE
-
+                                val requestOptions = RequestOptions()
+                                requestOptions.isMemoryCacheable
+                                requestOptions.placeholder(R.drawable.ic_baseline_image_24)
+                                requestOptions.error(R.drawable.ic_baseline_broken_image_24)
                                 Glide.with(this@MainActivity)
+                                    .setDefaultRequestOptions(requestOptions)
                                     .load(it)
+                                    .listener(object :RequestListener<Drawable>{
+                                        override fun onLoadFailed(
+                                            e: GlideException?,
+                                            model: Any?,
+                                            target: Target<Drawable>?,
+                                            isFirstResource: Boolean
+                                        ): Boolean {
+                                            Toast.makeText(this@MainActivity,"Load failed",Toast.LENGTH_SHORT).show()
+                                            return false
+                                        }
+
+                                        override fun onResourceReady(
+                                            resource: Drawable?,
+                                            model: Any?,
+                                            target: Target<Drawable>?,
+                                            dataSource: DataSource?,
+                                            isFirstResource: Boolean
+                                        ): Boolean {
+                                            return false
+                                        }
+                                    })
                                     .transition(DrawableTransitionOptions.withCrossFade())
                                     .into(main_iv)
                             }
